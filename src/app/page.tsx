5ca3e6dee224ddbbcc8955cf73b211c7bb1c7057
frn-af <91farhanalfathra@gmail.com>
2024-05-19 13:05:40 +0700

    feat: action button on table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,35 @@ import { column } from "@/components/column";
 import { DataTable } from "@/components/data-table";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { history, schedule } from "../../constants/seed";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-20">
+    <main className="flex min-h-screen flex-col items-center p-10">
       <div className="capitalize text-4xl font-bold">
         Sistem pemberian pakan otomatis
       </div>
       <div className="grid grid-cols-1 py-4 gap-4 md:grid-cols-2 w-full">
-        <Card className="flex flex-col p-4">
-          <CardHeader className="text-xl">Jadwal Pemberian Pakan</CardHeader>
+        <Card className="p-4">
+          <CardHeader>
+            <div className="flex justify-between">
+              <h2 className="text-xl">Jadwal Pemberian Pakan</h2>
+              <Button variant="secondary" className="space-x-2 w-40">
+                <PlusIcon className="h-6 w-6" />
+                <span>Tambah Jadwal</span>
+              </Button>
+            </div>
+          </CardHeader>
           <CardContent className="w-full">
             <DataTable columns={column} data={schedule} />
           </CardContent>
         </Card>
-        <Card className="flex flex-col p-4">
-          <CardHeader className="text-xl">History Pemberian Pakan</CardHeader>
+        <Card className="p-4">
+          <CardHeader>
+            <h2 className="text-xl">History Pemberian Pakan</h2>
+          </CardHeader>
           <CardContent className="w-full">
             <DataTable columns={column} data={history} />
           </CardContent>
--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { ColumnDef } from "@tanstack/react-table";
 import { Schedule } from "../../constants/seed";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
 
 export const column: ColumnDef<Schedule>[] = [
   {
@@ -10,13 +13,13 @@ export const column: ColumnDef<Schedule>[] = [
       const date = new Date(row.getValue("datetime"));
       const formatedDate = date.toLocaleDateString("id-ID", {
         year: "numeric",
-        month: "long",
+        month: "short",
         day: "numeric",
         hour: "2-digit",
         minute: "2-digit",
       });
 
-      return <div>{formatedDate}</div>;
+      return <div>{formatedDate} WIB</div>;
 
     }
   },
@@ -24,4 +27,35 @@ export const column: ColumnDef<Schedule>[] = [
     header: "Berat Pakan (gram)",
     accessorKey: "weight",
   },
+  {
+    id: "action",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="secondary"
+            >
+              <span className="sr-only">
+                Open Menu
+              </span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-40">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+
+
+    }
+  }
 ]
